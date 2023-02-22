ddcd8629cebd81ebb631d2a2738796e8174b0997
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2023-02-22 07:34:33 +0700

    minggu2

--- minggu2/app/src/main/java/com/example/minggu2/horizontalscrooll.java
+++ minggu2/app/src/main/java/com/example/minggu2/horizontalscrooll.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class horizontalscrooll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.activity_horizontalscrooll);
     }
 }
-
